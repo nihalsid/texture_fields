20a8580fb3350cd8532eddc5348c05505bbcb39f
Yawar Siddiqui <yawarnihal@gmail.com>
2020-05-20 01:59:12 +0200

    adjustments

--- scripts/evaluate_metrics.py
+++ scripts/evaluate_metrics.py
@@ -1,11 +1,12 @@
 import mesh2tex.utils.FID.fid_score as FID
 import mesh2tex.utils.FID.feature_l1 as feature_l1
 import mesh2tex.utils.SSIM_L1.ssim_l1_score as SSIM
+import os
 
 
 if __name__ == '__main__':
-    gt = "/home/yawar/baseline_renders/gt_224_plain"
-    tf = "/home/yawar/baseline_renders/pifu_224_plain/000"
+    gt = f"{os.environ['nihalsid']}/baseline_renders/gt_224"
+    tf = f"{os.environ['nihalsid']}/baseline_renders/our_224"
     print(SSIM.calculate_ssim_l1_given_paths([gt, tf], subfolder_mode=True))
     print(feature_l1.calculate_feature_l1_given_paths([gt, tf], 16, True, 2048, subfolder_mode=True))
     print(FID.calculate_fid_given_paths([gt, tf], 16, True, 2048, subfolder_mode=True))
